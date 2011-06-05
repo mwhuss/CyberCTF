b5638fba88e8292c32c032573ec2baea0b0122c0
Marshall Huss <mwhuss@gmail.com>
2011-06-04 22:26:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CyberCtf::Application.config.secret_token = 'c4f5d5b4e7e4563d027a350db365d768ab79d30bf43e3790d1441aa1f1f82912191cc5980c09925f76ebc27d08b3d1ac25646e4d60d66cb2650bd5d45078ab23'
